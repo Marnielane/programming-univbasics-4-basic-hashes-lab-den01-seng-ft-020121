20f14d5faf3624790485df092e544045005b64e9
Marnie Robinson <marnielane@gmail.com>
2021-01-12 21:43:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,9 @@ def pioneer
   }
 end
 
-
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  your_hash = {
+    :id => 4
+  }
+  your_hash[:id]
 end
